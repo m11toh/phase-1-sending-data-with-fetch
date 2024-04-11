d6a7cd014c6a4ee3f5d3965aabecb1e63643d483
m11toh <martohmashah@gmail.com>
2024-04-12 02:04:43 +0300

    fetch data

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Add your code here
+
+function submitData(name, email) {
+    const configurationObject = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        Accept: "application/json",
+      },
+      body: JSON.stringify({
+        name: `${name}`,
+        email: `${email}`,
+      }),
+    };
+  }
+  
+  return fetch("http://localhost:3000/users", configurationObject)
+    .then((res) => res.json)
+    .then(function (obj) {
+      document.write(obj.id);
+    })
+    .catch(function (err) {
+      alert("Bad things! Ragnarők!");
+      document.write(err.message);
+      console.log(err.message);
+    });
+  
+  submitData();
